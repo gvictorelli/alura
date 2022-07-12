3ba21434a041da015150836a2c20ffbeb3187813
Gregory Victorelli Soares <gregory.victorelli@mottu.com.br>
2022-07-12 17:29:56 -0300

    design patterns - strategy

---
+++ PadraoStrategy/PadraoStrategy/ICCC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadraoStrategy
+{
+    public class ICCC : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (orcamento.Valor < 1000.00)
+            {
+                return orcamento.Valor * 0.05;
+            }
+            else if(orcamento.Valor >= 1000.00 && orcamento.Valor <= 3000.00)
+            {
+                return (orcamento.Valor * 0.07);
+            }
+            
+            return (orcamento.Valor * 0.08) + 30;
+        }
+    }
+}
---
+++ PadraoStrategy/PadraoStrategy/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PadraoStrategy;
+
+Console.WriteLine("Hello, World!");
+
+var iss = new ISS();
+var icms = new ICMS();
+
+var orcamento = new Orcamento(20.0);
+
+CalculadorDeImposto calculador = new CalculadorDeImposto();
+calculador.RealizaCalculo(orcamento, iss);
+calculador.RealizaCalculo(orcamento, icms);
