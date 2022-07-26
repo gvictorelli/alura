2806ca9e77fe34055712cc1faa071ad5500d1dd3
Gregory Victorelli <gregory@gvictorelli.dev>
2022-07-25 22:10:39 -0300

    Implementacao formulario Nova Transferencia Angular

---
+++ PadraoStrategy/PadraoStrategy/Investimento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadraoStrategy
+{
+    public class Investimento
+    {
+        public double Valor { get; private set; }
+
+        public Investimento(double valor)
+        {
+            Valor = valor;
+        }
+    }
+}
---
+++ front/bytebank/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NovaTranferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTranferenciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
